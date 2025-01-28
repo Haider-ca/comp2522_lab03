304f504073e121f81064576348c304d284a248d4
Haider <halsudani@my.bcit.ca>
2025-01-28 15:02:17 -0800

    update IDevice class.

--- src/code/ca/bcit/comp2522/idevices/IDevice.java
+++ src/code/ca/bcit/comp2522/idevices/IDevice.java
@@ -3,19 +3,27 @@ package ca.bcit.comp2522.idevices;
 abstract class IDevice {
     private final String purpose;
 
-    protected IDevice(final String purpose) {
+    IDevice(final String purpose) {
+        validatePurpose(purpose);
         this.purpose = purpose;
     }
 
-    protected final String getPurpose() {
+    public String getPurpose() {
         return purpose;
     }
 
     protected abstract void printDetails();
 
     @Override
-    public final String toString() {
+    public String toString() {
         return "Purpose: " + purpose;
     }
+
+    protected static void validatePurpose(final String purpose) {
+        if (purpose == null || purpose.isBlank()) {
+            throw new IllegalArgumentException("Purpose cannot be null or blank.");
+        }
+    }
+
 }
 
--- src/code/ca/bcit/comp2522/idevices/IPod.java
+++ src/code/ca/bcit/comp2522/idevices/IPod.java
@@ -1,13 +1,101 @@
 package ca.bcit.comp2522.idevices;
 
-
 class IPod extends IDevice {
-    public IPod(String purpose) {
-        super(purpose);
+
+    private static final int MIN_NUMBER_OF_SONGS = 0;
+    private static final double MIN_MAX_VOLUME = 0.0;
+
+    private int numberOfSongs;
+    private double maxVolume;
+
+    IPod(final int numberOfSongs, final double maxVolume)
+    {
+        super("music");
+        validateNumberOfSongs(numberOfSongs);
+        validateMaxVolume(maxVolume);
+
+        this.numberOfSongs = numberOfSongs;
+        this.maxVolume = maxVolume;
+    }
+
+    public int getNumberOfSongs()
+    {
+        return numberOfSongs;
+    }
+
+    public void setNumberOfSongs(int numberOfSongs)
+    {
+        validateNumberOfSongs(numberOfSongs);
+        this.numberOfSongs = numberOfSongs;
+    }
+
+    public double getMaxVolume()
+    {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(double maxVolume)
+    {
+        validateMaxVolume(maxVolume);
+        this.maxVolume = maxVolume;
     }
 
     @Override
-    protected void printDetails() {
+    protected void printDetails()
+    {
+        System.out.println("IPod Details: " + toString());
+    }
 
+    @Override
+    public String toString()
+    {
+        StringBuilder builder = new StringBuilder(super.toString());
+
+        builder.append(", Songs: ")
+                .append(numberOfSongs)
+                .append(", Max Volume: ")
+                .append(maxVolume)
+                .append(" dB");
+        return builder.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null ||
+                getClass() != obj.getClass())
+        {
+            return false;
+        }
+
+        IPod ipod = (IPod) obj;
+        return numberOfSongs == ipod.numberOfSongs;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Integer.hashCode(numberOfSongs);
+    }
+
+    private static void validateNumberOfSongs(final int numberOfSongs)
+    {
+        if (numberOfSongs < MIN_NUMBER_OF_SONGS)
+        {
+            throw new IllegalArgumentException("Number of songs cannot be negative.");
+        }
+    }
+
+    private static void validateMaxVolume(final double maxVolume)
+    {
+        if (maxVolume <= MIN_MAX_VOLUME)
+        {
+            throw new IllegalArgumentException("Max volume must be a positive value.");
+        }
     }
 }
+
